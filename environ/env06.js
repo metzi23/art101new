c185d44a9af3c175b965a10b2f358b738580ea9e
metzi23 <michellelezama2006@gmail.com>
2025-11-18 20:25:15 -0800

    save jerry

---
+++ environ/env06.js
@@ -0,0 +1,111 @@
+//declaring an array with name nextEpisode
+let nextEpisode = {
+    title: "On the next episode of Jerry's Adventures...",
+    teaser: "Jerry finds himself face to face with a sabertooth tiger and uncharted territory"
+};
+
+//append the box to the bottom of page (after everything on top)
+$(document).ready( function() {
+    $(".yellow").append (`
+        <div id="nextEpisode">
+            <h3>${nextEpisode.title}</h3>
+            <p>${nextEpisode.teaser}</p>
+            <button id="nextBtn"> Go to Next Epsiode</button>
+        </div>
+    `);
+  
+    $("#nextBtn").hover(
+    function() {
+        $(this).css({
+      "background-color": "#a14d4dff"
+    });
+},
+    function() {
+        $(this).css({
+      "background-color": "#deddd7ff"
+    });
+    }
+);
+    $("#nextBtn").click (function () {
+    window.location.href = "env06_2.html"
+    });
+});
+
+
+
+const myAudio = document.getElementById ('characteraudio');
+myAudio.loop = true;
+
+$("#cartoonbtn").click(function () {
+if (myAudio.paused) {
+    myAudio.play ();
+    $(this).html("~Stop Music~");
+} else {
+    myAudio.pause();
+    $(this).html("Press me");
+}
+});
+
+// AUDIO BUTTON HOVER
+$("#cartoonbtn").hover(
+  function() {
+    // mouse enters
+    $(this).css({
+      "background-color": "#ffecbf"
+    });
+  },
+  function() {
+    $(this).css({
+      "background-color": "#efeee8"
+    });
+  }
+);
+
+function loopTigerPounce() {
+
+  // reset tiger to starting position each loop
+  $("#tiger2").css({ left: "-60px" });
+
+  // your pounce animation
+  $("#tiger2").animate(
+    {
+      left: "400px",    // where he lands
+    },
+    {
+      duration: 6000,
+      easing: "swing",
+      complete: function () {
+
+        // your shake animation
+        $("#tiger2")
+          .animate({ left: "580px" }, 100)
+          .animate({ left: "600px" }, 100, function () {
+
+            // restart animation after a pause
+            setTimeout(loopTigerPounce, 800);
+          });
+      }
+    }
+  );
+}
+
+// start the loop
+loopTigerPounce();
+
+
+let jerryFollow = false; // start NOT following
+
+// move jerry2 only if jerryFollow is true
+$(document).mousemove(function(event) {
+  if (jerryFollow) {
+    $("#jerry2").css({
+      left: event.pageX - 60,
+      top: event.pageY - 60
+    });
+  }
+});
+
+// click anywhere to toggle follow on/off
+$(document).click(function() {
+  jerryFollow = !jerryFollow;
+});
